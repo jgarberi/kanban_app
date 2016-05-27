fb181b8bc2debbd3a982d4dc4cabf3da281af347
Juan Garberi <jgarberi@gmail.com>
2016-05-26 21:57:45 -0300

    Setting Up Configuration Target for npm start and some logging on the console

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,10 @@ var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
-module.exports = {
+
+console.log("Lifecycle event is: " + TARGET);
+
+var common = {
     entry: APP_PATH,
     output: {
         path: BUILD_PATH,
@@ -23,17 +26,29 @@ module.exports = {
                 include: APP_PATH
             }
         ]
-    },
-    devServer: {
-        historyApiFallback: true,
-        hot: true,
-        inline: true,
-        progress: true
-    },
-    plugins: [
-        new webpack.HotModuleReplacementPlugin(),
-        new HtmlwebpackPlugin({
-            title: 'Kanban app'
-        })
-    ]
+    }
 };
+
+// Are we running outside npm?
+if(TARGET === 'start' || !TARGET) {
+
+    console.log("We are merging...: ");
+
+    module.exports = merge(common, {
+        devtool: 'eval-source-map',
+        devServer: {
+            historyApiFallback: true,
+            hot: true,
+            inline: true,
+            progress: true
+        },
+        plugins: [
+            new webpack.HotModuleReplacementPlugin(),
+            new HtmlwebpackPlugin({
+                title: 'Kanban app'
+            })
+        ]
+    });
+}
+
+console.log(module.exports);
